b275b3e6349fffc8e6bfa1904793a2c79735c331
hridayahmed <hridayahmed22@gmail.com>
2017-08-27 09:24:06 +0600

    android simple sms app

---
+++ app/src/main/java/com/appzgallery/newsms/MainActivity.java
@@ -0,0 +1,30 @@
+package com.appzgallery.newsms;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","01712679950",null));
+                intent.putExtra("sms body","Hello....");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
